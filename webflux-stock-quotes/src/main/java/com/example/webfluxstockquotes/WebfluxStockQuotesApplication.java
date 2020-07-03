332707261e056b0a7864be79e68de69e2faa425d
Marcin Kowalczyk <marcinkowalczyk24.7@gmail.com>
2020-07-03 16:40:10 +0200

    initial commit

---
+++ webflux-stock-quotes/src/main/java/com/example/webfluxstockquotes/WebfluxStockQuotesApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxstockquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStockQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxStockQuotesApplication.class, args);
+    }
+
+}
